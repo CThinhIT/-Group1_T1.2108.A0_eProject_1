68a8efb9af4cf5cd000080b20e14ccd0ba37654c
CThinhIT <thinhtcit03@gmail.com>
2021-12-02 20:49:18 +0700

    update detail

---
+++ js/product.js
@@ -0,0 +1,346 @@
+var productS = [{
+    "name": "Washing machine LG Inverter 8.5kg FV1408G4W",
+    "id": "01",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/MG/mgLG/mgLG1/1.jpg",
+    "pic2": "../asset/imgProduct/MG/mgLG/mgLG1/2.jpg",
+    "pic3": "../asset/imgProduct/MG/mgLG/mgLG1/3.jpg",
+    "pic4": "../asset/imgProduct/MG/mgLG/mgLG1/4.jpg",
+    "pic5": "../asset/imgProduct/MG/mgLG/mgLG1/5.jpg",
+    "pic6": "../asset/imgProduct/MG/mgLG/mgLG1/6.jpg"
+}, {
+    "name": "Air-conditioner LG Inverter 1.5 HPV13ENH",
+    "id": "02",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/ML/mlLG/mlLG1/1.jpg",
+    "pic2": "../asset/imgProduct/ML/mlLG/mlLG1/2.jpg",
+    "pic3": "../asset/imgProduct/ML/mlLG/mlLG1/3.jpg",
+    "pic4": "../asset/imgProduct/ML/mlLG/mlLG1/4.jpg",
+    "pic5": "../asset/imgProduct/ML/mlLG/mlLG1/5.jpg",
+    "pic6": "../asset/imgProduct/ML/mlLG/mlLG1/6.jpg"
+}, {
+    "name": "Fridge LG Inverter 394 liter GN-D392BLA",
+    "id": "03",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/TL/tlLG/tlLG1/1.jpg",
+    "pic2": "../asset/imgProduct/TL/tlLG/tlLG1/2.jpg",
+    "pic3": "../asset/imgProduct/TL/tlLG/tlLG1/3.jpg",
+    "pic4": "../asset/imgProduct/TL/tlLG/tlLG1/4.jpg",
+    "pic5": "../asset/imgProduct/TL/tlLG/tlLG1/5.jpg",
+    "pic6": "../asset/imgProduct/TL/tlLG/tlLG1/6.jpg"
+
+}, {
+    "name": "Smart Television OLED LG 4K 65 inch 65B1PTA",
+    "id": "04",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/TV/tvLG/tvLG1/1.jpg",
+    "pic2": "../asset/imgProduct/TV/tvLG/tvLG1/2.jpg",
+    "pic3": "../asset/imgProduct/TV/tvLG/tvLG1/3.jpg",
+    "pic4": "../asset/imgProduct/TV/tvLG/tvLG1/4.jpg",
+    "pic5": "../asset/imgProduct/TV/tvLG/tvLG1/5.jpg",
+    "pic6": "../asset/imgProduct/TV/tvLG/tvLG1/6.jpg"
+}, {
+    "name": "Washing machine Inverter 9kh FM1209N6W",
+    "id": "05",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/MG/mgLG/mgLG2/1.jpg",
+    "pic2": "../asset/imgProduct/MG/mgLG/mgLG2/2.jpg",
+    "pic3": "../asset/imgProduct/MG/mgLG/mgLG2/3.jpg",
+    "pic4": "../asset/imgProduct/MG/mgLG/mgLG2/4.jpg",
+    "pic5": "../asset/imgProduct/MG/mgLG/mgLG2/5.jpg",
+    "pic6": "../asset/imgProduct/MG/mgLG/mgLG2/6.jpg"
+}, {
+    "name": "Fridge LG Inverter 494 liter GR-D22MB",
+    "id": "06",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/TL/tlLG/tlLG3/1.jpg",
+    "pic2": "../asset/imgProduct/TL/tlLG/tlLG3/2.jpg",
+    "pic3": "../asset/imgProduct/TL/tlLG/tlLG3/3.jpg",
+    "pic4": "../asset/imgProduct/TL/tlLG/tlLG3/4.jpg",
+    "pic5": "../asset/imgProduct/TL/tlLG/tlLG3/5.jpg",
+    "pic6": "../asset/imgProduct/TL/tlLG/tlLG3/6.jpg"
+}];
+
+var productF = [{
+    "name": "Washing machine LG Inverter 8.5kg FV1408G4W",
+    "id": "07",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/MG/mgPa/mgPa1/1.jpg",
+    "pic2": "../asset/imgProduct/MG/mgPa/mgPa1/2.jpg",
+    "pic3": "../asset/imgProduct/MG/mgPa/mgPa1/3.jpg",
+    "pic4": "../asset/imgProduct/MG/mgPa/mgPa1/4.jpg",
+    "pic5": "../asset/imgProduct/MG/mgPa/mgPa1/5.jpg",
+    "pic6": "../asset/imgProduct/MG/mgPa/mgPa1/6.jpg"
+}, {
+    "name": "Air-conditioner LG Inverter 1.5 HPV13ENH",
+    "id": "08",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/ML/mlSS/mlSS1/1.jpg",
+    "pic2": "../asset/imgProduct/ML/mlSS/mlSS1/2.jpg",
+    "pic3": "../asset/imgProduct/ML/mlSS/mlSS1/3.jpg",
+    "pic4": "../asset/imgProduct/ML/mlSS/mlSS1/4.jpg",
+    "pic5": "../asset/imgProduct/ML/mlSS/mlSS1/5.jpg",
+    "pic6": "../asset/imgProduct/ML/mlSS/mlSS1/6.jpg"
+}, {
+    "name": "Fridge LG Inverter 394 liter GN-D392BLA",
+    "id": "09",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/TL/tlSS/tlSS1/1.jpg",
+    "pic2": "../asset/imgProduct/TL/tlSS/tlSS1/2.jpg",
+    "pic3": "../asset/imgProduct/TL/tlSS/tlSS1/3.jpg",
+    "pic4": "../asset/imgProduct/TL/tlSS/tlSS1/4.jpg",
+    "pic5": "../asset/imgProduct/TL/tlSS/tlSS1/5.jpg",
+    "pic6": "../asset/imgProduct/TL/tlSS/tlSS1/6.jpg"
+
+}, {
+    "name": "Smart Television OLED LG 4K 65 inch 65B1PTA",
+    "id": "10",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/TV/tvLG/tvLG3/1.jpg",
+    "pic2": "../asset/imgProduct/TV/tvLG/tvLG3/2.jpg",
+    "pic3": "../asset/imgProduct/TV/tvLG/tvLG3/3.jpg",
+    "pic4": "../asset/imgProduct/TV/tvLG/tvLG3/4.jpg",
+    "pic5": "../asset/imgProduct/TV/tvLG/tvLG3/5.jpg",
+    "pic6": "../asset/imgProduct/TV/tvLG/tvLG3/6.jpg"
+}, {
+    "name": "Washing machine Inverter 9kh FM1209N6W",
+    "id": "11",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/MG/mgLG/mgLG2/1.jpg",
+    "pic2": "../asset/imgProduct/MG/mgLG/mgLG2/2.jpg",
+    "pic3": "../asset/imgProduct/MG/mgLG/mgLG2/3.jpg",
+    "pic4": "../asset/imgProduct/MG/mgLG/mgLG2/4.jpg",
+    "pic5": "../asset/imgProduct/MG/mgLG/mgLG2/5.jpg",
+    "pic6": "../asset/imgProduct/MG/mgLG/mgLG2/6.jpg"
+}, {
+    "name": "Fridge LG Inverter 494 liter GR-D22MB",
+    "id": "12",
+    "price": 10000,
+    "pic1": "../asset/imgProduct/TL/tlLG/tlLG3/1.jpg",
+    "pic2": "../asset/imgProduct/TL/tlLG/tlLG3/2.jpg",
+    "pic3": "../asset/imgProduct/TL/tlLG/tlLG3/3.jpg",
+    "pic4": "../asset/imgProduct/TL/tlLG/tlLG3/4.jpg",
+    "pic5": "../asset/imgProduct/TL/tlLG/tlLG3/5.jpg",
+    "pic6": "../asset/imgProduct/TL/tlLG/tlLG3/6.jpg"
+}];
+
+var res = '';
+var res1 = '';
+
+function getPageUrlId() {
+    var urlID = window.location.href;
+    res = urlID.slice(-3);
+    res1 = urlID.slice(-1);
+}
+
+function displayImageF(pDuctF) {
+    let y = ``;
+    $.each(pDuctF, function (k, v) {
+        y += `<div class="featuredP1">
+        <div class="imgFP1">
+            <a href="../productDetail/detail.html?key=${v.id}"><img src="${v.pic1}" alt="" loading="lazy" class="imgProduct">
+        </div>
+        <div class="compare-buyFP">
+            <div class="row">
+                <div class="col-sm-6">
+                    <a href="" class="btn btn-lg btn-block">Compare</a>
+                </div>
+                <div class="col-sm-6"><a href="" class="btn btn-lg btn-block">Buy</a></div>
+            </div>
+        </div>
+    </div>`;
+    })
+
+    $('#spNoibat').html(y);
+}
+displayImageF(productF);
+
+function showDetailF(pDetail) {
+    let s = ``;
+    var urlID = window.location.href;
+    res = urlID.slice(-3);
+    res1 = urlID.slice(-2);
+    $.each(pDetail, function (k, v) {
+        if (v.id == res1) {
+            s += `<div class="container">
+            <div class="row">
+            <!--IMG product-->
+            <div class="col-7">
+                <div class="tab-content">
+                    <div id="pic1" class="container tab-pane active"><br>
+                        <img src="${v.pic1}" alt="" class="imgDetail">
+                    </div>
+                    <div id="pic2" class="container tab-pane fade"><br>
+                        <img src="${v.pic2}" alt="" class="imgDetail">
+                    </div>
+                    <div id="pic3" class="container tab-pane fade"><br>
+                        <img src="${v.pic3}" alt="" class="imgDetail">
+                    </div>
+                    <div id="pic4" class="container tab-pane fade"><br>
+                        <img src="${v.pic4}" alt="" class="imgDetail">
+                    </div>
+                    <div id="pic5" class="container tab-pane fade"><br>
+                        <img src="${v.pic5}" alt="" class="imgDetail">
+                    </div>
+                </div>
+                <ul class="nav nav-pills" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="pill" href="#pic1"><img
+                                src="${v.pic1}" alt="" width="80px" height="50px"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#pic2"><img
+                                src="${v.pic2}" alt="" width="80px" height="50px"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#pic3"><img
+                                src="${v.pic3}" alt="" width="80px" height="50px"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#pic4"><img
+                                src="${v.pic4}" alt="" width="80px" height="50px"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#pic5"><img
+                                src="${v.pic5}" alt="" width="80px" height="50px"></a>
+                    </li>
+                </ul>
+            </div>
+            <!--Thong tin product-->
+            <div class="col-5">
+                <div class="gioithieuSP">
+                    <div class="tenSP">
+                        <h2>${v.name}</h2>
+                    </div>
+                    <div class="thongsoSP">
+
+                    </div>
+                    <div class="giaSP">
+                        <h3>Price: $${v.price}</h3>
+                    </div>
+                    <div class="addToCart">
+                        <a href="#" class="btn btn-dark">Add to cart</a>
+                    </div>
+                    <br>
+                    <div class="compare">
+                        <a href="#" class="btn btn-dark">Compare</a>
+                    </div>
+                    <br>
+                    <div class="addRview">
+                        <a href="#" class="btn btn-dark">Add review</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`;
+        }
+    })
+    $('.detailProduct1').html(s);
+}
+showDetailF(productF);
+
+function displayImageS(pDuctH) {
+    let x = ``;
+    $.each(pDuctH, function (k, v) {
+        x += `<div class="featuredP1">
+        <div class="imgFP1">
+            <a href="../productDetail/detail.html?key=${v.id}"><img src="${v.pic1}" alt="" loading="lazy">
+        </div>
+        <div class="compare-buyFP">
+            <div class="row">
+                <div class="col-sm-6">
+                    <a href="" class="btn btn-lg btn-block">Compare</a>
+                </div>
+                <div class="col-sm-6"><a href="" class="btn btn-lg btn-block">Buy</a></div>
+            </div>
+        </div>
+    </div>`;
+    })
+
+    $('#sanphambanchay').html(x);
+}
+displayImageS(productS);
+
+function showDetail(pDetail) {
+    let s = ``;
+    var urlID = window.location.href;
+    res = urlID.slice(-3);
+    res1 = urlID.slice(-2);
+    $.each(pDetail, function (k, v) {
+        if (v.id == res1) {
+            s += `<div class="container">
+            <div class="row">
+            <!--IMG product-->
+            <div class="col-7">
+                <div class="tab-content">
+                    <div id="pic1" class="container tab-pane active"><br>
+                        <img src="${v.pic1}" alt="" class="imgDetail">
+                    </div>
+                    <div id="pic2" class="container tab-pane fade"><br>
+                        <img src="${v.pic2}" alt="" class="imgDetail">
+                    </div>
+                    <div id="pic3" class="container tab-pane fade"><br>
+                        <img src="${v.pic3}" alt="" class="imgDetail">
+                    </div>
+                    <div id="pic4" class="container tab-pane fade"><br>
+                        <img src="${v.pic4}" alt="" class="imgDetail">
+                    </div>
+                    <div id="pic5" class="container tab-pane fade"><br>
+                        <img src="${v.pic5}" alt="" class="imgDetail">
+                    </div>
+                </div>
+                <ul class="nav nav-pills" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="pill" href="#pic1"><img
+                                src="${v.pic1}" alt="" width="80px" height="50px"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#pic2"><img
+                                src="${v.pic2}" alt="" width="80px" height="50px"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#pic3"><img
+                                src="${v.pic3}" alt="" width="80px" height="50px"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#pic4"><img
+                                src="${v.pic4}" alt="" width="80px" height="50px"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#pic5"><img
+                                src="${v.pic5}" alt="" width="80px" height="50px"></a>
+                    </li>
+                </ul>
+            </div>
+            <!--Thong tin product-->
+            <div class="col-5">
+                <div class="gioithieuSP">
+                    <div class="tenSP">
+                        <h2>${v.name}</h2>
+                    </div>
+                    <div class="thongsoSP">
+
+                    </div>
+                    <div class="giaSP">
+                        <h3>Price: $${v.price}</h3>
+                    </div>
+                    <div class="addToCart">
+                        <a href="#" class="btn btn-dark">Add to cart</a>
+                    </div>
+                    <br>
+                    <div class="compare">
+                        <a href="#" class="btn btn-dark">Compare</a>
+                    </div>
+                    <br>
+                    <div class="addRview">
+                        <a href="#" class="btn btn-dark">Add review</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`;
+        }
+    })
+    $('.detailProduct').html(s);
+}
+showDetail(productS);
