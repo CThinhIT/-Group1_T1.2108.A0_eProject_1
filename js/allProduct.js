1fa53e8ca000249824cec5f99559958ba29ff5bc
CThinhIT <thinhtcit03@gmail.com>
2021-12-06 13:42:01 +0700

    no message

--- js/allProduct.js
+++ js/allProduct.js
@@ -131,13 +131,218 @@ var productTV = [{
         "pic5": "../asset/imgProduct/TV/tvSS/tvSS6/5.jpg"
     }
 ]
+
+var productTL=[
+    {
+        "name":"Fridge LG Inverter 394 liters GN-D392BLA",
+        "id":"01",
+        "brand":"a",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlLG/tlLG1/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlLG/tlLG1/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlLG/tlLG1/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlLG/tlLG1/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlLG/tlLG1/5.jpg"
+    },
+    {
+        "name":"Fridge LG Inverter 374 liters GN-D372BLA",
+        "id":"02",
+        "brand":"a",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlLG/tlLG2/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlLG/tlLG2/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlLG/tlLG2/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlLG/tlLG2/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlLG/tlLG2/5.jpg"
+    },
+    {
+        "name":"Fridge LG Inverter 494 liters GR-D22MB",
+        "id":"03",
+        "brand":"a",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlLG/tlLG3/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlLG/tlLG3/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlLG/tlLG3/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlLG/tlLG3/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlLG/tlLG3/5.jpg"
+    },
+    {
+        "name":"Fridge LG Inverter 496 liters GR-X22MB",
+        "id":"04",
+        "brand":"a",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlLG/tlLG4/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlLG/tlLG4/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlLG/tlLG4/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlLG/tlLG4/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlLG/tlLG4/5.jpg"
+    },
+    {
+        "name":"Fridge LG Inverter 613 liters GR-B247JDS",
+        "id":"05",
+        "brand":"a",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlLG/tlLG5/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlLG/tlLG5/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlLG/tlLG5/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlLG/tlLG5/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlLG/tlLG5/5.jpg"
+    },
+    {
+        "name":"Fridge LG Inverter 601 liters GR-X247MC",
+        "id":"06",
+        "brand":"a",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlLG/tlLG6/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlLG/tlLG6/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlLG/tlLG6/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlLG/tlLG6/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlLG/tlLG6/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 380 liters RT38K5982BSSV",
+        "id":"01",
+        "brand":"b",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlSS/tlSS1/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlSS/tlSS1/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlSS/tlSS1/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlSS/tlSS1/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlSS/tlSS1/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 647 liters RS62R5001M9SV",
+        "id":"02",
+        "brand":"b",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlSS/tlSS2/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlSS/tlSS2/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlSS/tlSS2/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlSS/tlSS2/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlSS/tlSS2/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 647 liters RS62R5001B4SV",
+        "id":"03",
+        "brand":"b",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlSS/tlSS3/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlSS/tlSS3/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlSS/tlSS3/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlSS/tlSS3/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlSS/tlSS3/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 488 liters RF48A4000B4SV",
+        "id":"04",
+        "brand":"b",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlSS/tlSS4/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlSS/tlSS4/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlSS/tlSS4/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlSS/tlSS4/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlSS/tlSS4/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 488 liters RF48A4010M9SV",
+        "id":"05",
+        "brand":"b",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlSS/tlSS5/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlSS/tlSS5/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlSS/tlSS5/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlSS/tlSS5/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlSS/tlSS5/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 236 liters RT22M4032BYSV",
+        "id":"06",
+        "brand":"b",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlSS/tlSS6/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlSS/tlSS6/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlSS/tlSS6/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlSS/tlSS6/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlSS/tlSS6/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 380 liters RT38K5982BSSV",
+        "id":"01",
+        "brand":"c",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlPa/tlPa1/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlPa/tlPa1/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlPa/tlPa1/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlPa/tlPa1/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlPa/tlPa1/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 647 liters RS62R5001M9SV",
+        "id":"02",
+        "brand":"c",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlPa/tlPa2/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlPa/tlPa2/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlPa/tlPa2/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlPa/tlPa2/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlPa/tlPa2/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 647 liters RS62R5001B4SV",
+        "id":"03",
+        "brand":"c",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlPa/tlPa3/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlPa/tlPa3/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlPa/tlPa3/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlPa/tlPa3/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlPa/tlPa3/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 488 liters RF48A4000B4SV",
+        "id":"04",
+        "brand":"c",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlPa/tlPa4/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlPa/tlPa4/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlPa/tlPa4/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlPa/tlPa4/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlPa/tlPa4/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 488 liters RF48A4010M9SV",
+        "id":"05",
+        "brand":"c",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlPa/tlPa5/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlPa/tlPa5/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlPa/tlPa5/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlPa/tlPa5/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlPa/tlPa5/5.jpg"
+    },
+    {
+        "name":"Fridge Samsung Inverter 236 liters RT22M4032BYSV",
+        "id":"06",
+        "brand":"c",
+        "price":1000,
+        "pic1": "../asset/imgProduct/TL/tlPa/tlPa6/1.jpg",
+        "pic2": "../asset/imgProduct/TL/tlPa/tlPa6/2.jpg",
+        "pic3": "../asset/imgProduct/TL/tlPa/tlPa6/3.jpg",
+        "pic4": "../asset/imgProduct/TL/tlPa/tlPa6/4.jpg",
+        "pic5": "../asset/imgProduct/TL/tlPa/tlPa6/5.jpg"
+    }
+]
 var res = "";
 var res1 = "";
 
-function showProduct(product) {
+function showProduct1(product) {
     let x = ``;
+    var urlID = window.location.href;
+    res1 = urlID.slice(-1);
     $.each(product, function (k, v) {
-        x += `<a href="../productDetail/detailTV.html?key=${v.brand}${v.id}"><div class="col-sm-4">
+        if (res1 == "l") {
+            if (v.brand == "a")
+                x += `<a href="../productDetail/detailTV.html?key=${v.brand}${v.id}"><div class="col-sm-4">
         <img src="${v.pic1}" alt="" class="imgProductA">
         <div class="col-sm-12">
                 <h6>${v.name}</h6>
@@ -154,20 +359,128 @@ function showProduct(product) {
             </div>
         </div>
     </div>`;
-    })
-    $("#tatcasp").html(x);
+        } else if (res1 == "s") {
+            if (v.brand == "b")
+                x += `<a href="../productDetail/detailTV.html?key=${v.brand}${v.id}"><div class="col-sm-4">
+        <img src="${v.pic1}" alt="" class="imgProductA">
+        <div class="col-sm-12">
+                <h6>${v.name}</h6>
+                </div>
+                </a>
+        <div class="compare-buyFP">
+            <div class="row">
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Compare</a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Buy</a>
+                    </div>
+            </div>
+        </div>
+    </div>`;
+        }
+        else if(res1=="d"){
+            x += `<a href="../productDetail/detailTV.html?key=${v.brand}${v.id}"><div class="col-sm-4">
+        <img src="${v.pic1}" alt="" class="imgProductA">
+        <div class="col-sm-12">
+                <h6>${v.name}</h6>
+                </div>
+                </a>
+        <div class="compare-buyFP">
+            <div class="row">
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Compare</a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Buy</a>
+                    </div>
+            </div>
+        </div>
+    </div>`;
+        }
+    }) 
+    $("#tatcaspTV").html(x);
 }
-showProduct(productTV);
+showProduct1(productTV);
 
-function showDetail(proDuct) {
+
+function showProduct2(product) {
+    let x = ``;
+    var urlID = window.location.href;
+    res1 = urlID.slice(-1);
+    $.each(product, function (k, v) {
+        if (res1 == "l") {
+            if (v.brand == "a")
+                x += `<a href="../productDetail/detailTV.html?key=${v.brand}${v.id}"><div class="col-sm-4">
+        <img src="${v.pic1}" alt="" class="imgProductA">
+        <div class="col-sm-12">
+                <h6>${v.name}</h6>
+                </div>
+                </a>
+        <div class="compare-buyFP">
+            <div class="row">
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Compare</a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Buy</a>
+                    </div>
+            </div>
+        </div>
+    </div>`;
+        } else if (res1 == "s") {
+            if (v.brand == "b")
+                x += `<a href="../productDetail/detailTV.html?key=${v.brand}${v.id}"><div class="col-sm-4">
+        <img src="${v.pic1}" alt="" class="imgProductA">
+        <div class="col-sm-12">
+                <h6>${v.name}</h6>
+                </div>
+                </a>
+        <div class="compare-buyFP">
+            <div class="row">
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Compare</a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Buy</a>
+                    </div>
+            </div>
+        </div>
+    </div>`;
+        }
+        else if(res1=="d"){
+            x += `<a href="../productDetail/detailTL.html?key=${v.brand}${v.id}"><div class="col-sm-4">
+        <img src="${v.pic1}" alt="" class="imgProductA">
+        <div class="col-sm-12">
+                <h6>${v.name}</h6>
+                </div>
+                </a>
+        <div class="compare-buyFP">
+            <div class="row">
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Compare</a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="" class="btn btn-lg btn-block">Buy</a>
+                    </div>
+            </div>
+        </div>
+    </div>`;
+        }
+    }) 
+    $("#tatcaspTL").html(x);
+}
+showProduct2(productTL);
+
+function showDetail2(proDuct2) {
     var urlID = window.location.href;
     res = urlID.slice(-3, -2);
     res1 = urlID.slice(-2);
     let s = ``;
-    $.each(proDuct, function (k, v) {
-        if(v.brand==res){
-            if(v.id==res1){
-                s+=`<div class="container">
+    $.each(proDuct2, function (k, v) {
+        if (v.brand == res) {
+            if (v.id == res1) {
+                s += `<div class="container">
                 <div class="row">
                 <!--IMG product-->
                 <div class="col-7">
@@ -241,6 +554,92 @@ function showDetail(proDuct) {
             }
         }
     })
-    $(".detailProduct").html(s);
+    $(".detailProductTL").html(s);
+}
+showDetail2(productTL);
+
+function showDetail(proDuct) {
+    var urlID = window.location.href;
+    res = urlID.slice(-3, -2);
+    res1 = urlID.slice(-2);
+    let s = ``;
+    $.each(proDuct, function (k, v) {
+        if (v.brand == res) {
+            if (v.id == res1) {
+                s += `<div class="container">
+                <div class="row">
+                <!--IMG product-->
+                <div class="col-7">
+                    <div class="tab-content">
+                        <div id="pic1" class="container tab-pane active"><br>
+                            <img src="${v.pic1}" alt="" class="imgDetail">
+                        </div>
+                        <div id="pic2" class="container tab-pane fade"><br>
+                            <img src="${v.pic2}" alt="" class="imgDetail">
+                        </div>
+                        <div id="pic3" class="container tab-pane fade"><br>
+                            <img src="${v.pic3}" alt="" class="imgDetail">
+                        </div>
+                        <div id="pic4" class="container tab-pane fade"><br>
+                            <img src="${v.pic4}" alt="" class="imgDetail">
+                        </div>
+                        <div id="pic5" class="container tab-pane fade"><br>
+                            <img src="${v.pic5}" alt="" class="imgDetail">
+                        </div>
+                    </div>
+                    <ul class="nav nav-pills" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="pill" href="#pic1"><img
+                                    src="${v.pic1}" alt="" width="80px" height="50px"></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="pill" href="#pic2"><img
+                                    src="${v.pic2}" alt="" width="80px" height="50px"></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="pill" href="#pic3"><img
+                                    src="${v.pic3}" alt="" width="80px" height="50px"></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="pill" href="#pic4"><img
+                                    src="${v.pic4}" alt="" width="80px" height="50px"></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="pill" href="#pic5"><img
+                                    src="${v.pic5}" alt="" width="80px" height="50px"></a>
+                        </li>
+                    </ul>
+                </div>
+                <!--Thong tin product-->
+                <div class="col-5">
+                    <div class="gioithieuSP">
+                        <div class="tenSP">
+                            <h2>${v.name}</h2>
+                        </div>
+                        <div class="thongsoSP">
+    
+                        </div>
+                        <div class="giaSP">
+                            <h3>Price: $${v.price}</h3>
+                        </div>
+                        <div class="addToCart">
+                            <a href="#" class="btn btn-dark">Add to cart</a>
+                        </div>
+                        <br>
+                        <div class="compare">
+                            <a href="#" class="btn btn-dark">Compare</a>
+                        </div>
+                        <br>
+                        <div class="addRview">
+                            <a href="#" class="btn btn-dark">Add review</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+            }
+        }
+    })
+    $(".detailProductTV").html(s);
 }
 showDetail(productTV);
