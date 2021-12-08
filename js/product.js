8b0cd72f2b0145d663d39641286fc93f48cabf82
CThinhIT <thinhtcit03@gmail.com>
2021-12-09 01:07:40 +0700

    done

--- js/product.js
+++ js/product.js
@@ -1,18 +1,24 @@
 var productS = [{
-    "name": "Washing machine LG Inverter 8.5kg FV1408G4W",
-    "nameC" :"Washing&nbspmachine&nbspLG&nbspInverter&nbspFV1408G4W",
+    "name": "Washing machine LG 8.5kg FV1408G4W",
+    "nameC": "Washing&nbspmachine&nbspLG&nbspInverter&nbspFV1408G4W",
     "id": "01",
-    "price": 10000,
+    "size" : "Height 85 cm - Width 60 cm - Depth 55 cm - Weight 69 kg",
+    "year": 2021,
+    "brandS": "LG",
+    "price": 5576,
     "pic1": "../asset/imgProduct/MG/mgLG/mgLG1/1.jpg",
     "pic2": "../asset/imgProduct/MG/mgLG/mgLG1/2.jpg",
     "pic3": "../asset/imgProduct/MG/mgLG/mgLG1/3.jpg",
     "pic4": "../asset/imgProduct/MG/mgLG/mgLG1/4.jpg",
     "pic5": "../asset/imgProduct/MG/mgLG/mgLG1/5.jpg"
 }, {
-    "name": "Air-conditioner LG Inverter 1.5 HPV13ENH",
-    "nameC" :"Air-conditioner&nbspLG&nbspInverter&nbspHPV13ENH",
+    "name": "Air-conditioner LG 1.5 HPV13ENH",
+    "nameC": "Air-conditioner&nbspLG&nbspInverter&nbspHPV13ENH",
     "id": "02",
-    "price": 10000,
+    "size" : "Length 83.7 cm - Height 30.8 cm - Thickness 19 cm - Weight 8.7 kg",
+    "year": 2021,
+    "brandS": "LG",
+    "price": 470004,
     "pic1": "../asset/imgProduct/ML/mlLG/mlLG1/1.jpg",
     "pic2": "../asset/imgProduct/ML/mlLG/mlLG1/2.jpg",
     "pic3": "../asset/imgProduct/ML/mlLG/mlLG1/3.jpg",
@@ -20,9 +26,12 @@ var productS = [{
     "pic5": "../asset/imgProduct/ML/mlLG/mlLG1/5.jpg"
 }, {
     "name": "Fridge LG Inverter 394 liter GN-D392BLA",
-    "nameC" :"Fridge&nbspLG&nbspInverter&nbspGN-D392BL",
+    "nameC": "Fridge&nbspLG&nbspInverter&nbspGN-D392BL",
     "id": "03",
-    "price": 10000,
+    "size" : "Height 85 cm - Width 60 cm - Depth 55 cm - Weight 69 kg",
+    "year": 2020,
+    "brandS": "LG",
+    "price": 841174,
     "pic1": "../asset/imgProduct/TL/tlLG/tlLG1/1.jpg",
     "pic2": "../asset/imgProduct/TL/tlLG/tlLG1/2.jpg",
     "pic3": "../asset/imgProduct/TL/tlLG/tlLG1/3.jpg",
@@ -30,20 +39,26 @@ var productS = [{
     "pic5": "../asset/imgProduct/TL/tlLG/tlLG1/5.jpg"
 
 }, {
-    "name": "Smart Television OLED LG 4K 65 inch 65B1PTA",
-    "nameC" :"Television&nbspLG&nbsp65B1PTA",
+    "name": "Television OLED LG 4K 65 inch 65B1PTA",
+    "nameC": "Television&nbspLG&nbsp65B1PTA",
     "id": "04",
-    "price": 10000,
+    "size" : "65 inch",
+    "year": 2019,
+    "brandS": "LG",
+    "price": 871498,
     "pic1": "../asset/imgProduct/TV/tvLG/tvLG1/1.jpg",
     "pic2": "../asset/imgProduct/TV/tvLG/tvLG1/2.jpg",
     "pic3": "../asset/imgProduct/TV/tvLG/tvLG1/3.jpg",
     "pic4": "../asset/imgProduct/TV/tvLG/tvLG1/4.jpg",
     "pic5": "../asset/imgProduct/TV/tvLG/tvLG1/5.jpg"
 }, {
-    "name": "Washing machine LG Inverter 9kh FM1209N6W",
-    "nameC" :"Washing&nbspmachine&nbspLG&nbspInverter&nbspFM1209N6W",
+    "name": "Washing machine LG 9kh FM1209N6W",
+    "nameC": "Washing&nbspmachine&nbspLG&nbspInverter&nbspFM1209N6W",
     "id": "05",
-    "price": 10000,
+    "size" : "Height 85 cm - Width 60 cm - Depth 55 cm - Weight 69 kg",
+    "year": 2020,
+    "brandS": "LG",
+    "price": 38762,
     "pic1": "../asset/imgProduct/MG/mgLG/mgLG2/1.jpg",
     "pic2": "../asset/imgProduct/MG/mgLG/mgLG2/2.jpg",
     "pic3": "../asset/imgProduct/MG/mgLG/mgLG2/3.jpg",
@@ -51,9 +66,12 @@ var productS = [{
     "pic5": "../asset/imgProduct/MG/mgLG/mgLG2/5.jpg"
 }, {
     "name": "Fridge LG Inverter 494 liter GR-D22MB",
-    "nameC" :"Fridge&nbspLG&nbspInverter&nbspGR-D22MB",
+    "nameC": "Fridge&nbspLG&nbspInverter&nbspGR-D22MB",
     "id": "06",
-    "price": 10000,
+    "size" : "Height 85 cm - Width 60 cm - Depth 55 cm - Weight 69 kg",
+    "year": 2020,
+    "brandS": "LG",
+    "price": 1941735,
     "pic1": "../asset/imgProduct/TL/tlLG/tlLG3/1.jpg",
     "pic2": "../asset/imgProduct/TL/tlLG/tlLG3/2.jpg",
     "pic3": "../asset/imgProduct/TL/tlLG/tlLG3/3.jpg",
@@ -62,30 +80,33 @@ var productS = [{
 }];
 
 var productF = [{
-    "name": "Fridge Panasonic Inverter 417 liters NR-BX471GPKV",
-    "nameC" :"Fridge&nbspPanasonic&nbspInverter&nbspNR-BX471GPKV",
+    "name": "Fridge Panasonic 417 liters NR-BX471GPKV",
+    "nameC": "Fridge&nbspPanasonic&nbspInverter&nbspNR-BX471GPKV",
     "id": "07",
-    "price": 10000,
+    "brandS": "Panasonic",
+    "price": 884333,
     "pic1": "../asset/imgProduct/MG/mgPa/mgPa1/1.jpg",
     "pic2": "../asset/imgProduct/MG/mgPa/mgPa1/2.jpg",
     "pic3": "../asset/imgProduct/MG/mgPa/mgPa1/3.jpg",
     "pic4": "../asset/imgProduct/MG/mgPa/mgPa1/4.jpg",
     "pic5": "../asset/imgProduct/MG/mgPa/mgPa1/5.jpg"
 }, {
-    "name": "Air-Conditioner Samsung Inverter 1 HP AR09TYHQASINSV",
-    "nameC" :"Air-Conditioner&nbspSamsung&nbspInverter&nbspAR09TYHQASINSV",
+    "name": "Air-Conditioner Samsung 1 HP AR09TYHQA",
+    "nameC": "Air-Conditioner&nbspSamsung&nbspInverter&nbspAR09TYHQASINSV",
     "id": "08",
-    "price": 10000,
+    "brandS": "SamSung",
+    "price": 388002,
     "pic1": "../asset/imgProduct/ML/mlSS/mlSS1/1.jpg",
     "pic2": "../asset/imgProduct/ML/mlSS/mlSS1/2.jpg",
     "pic3": "../asset/imgProduct/ML/mlSS/mlSS1/3.jpg",
     "pic4": "../asset/imgProduct/ML/mlSS/mlSS1/4.jpg",
     "pic5": "../asset/imgProduct/ML/mlSS/mlSS1/5.jpg"
 }, {
-    "name": "Fridge Samsung Inverter 380 liters RT38K5982BSSV",
-    "nameC" :"Fridge&nbspSamsung&nbspInverter&nbspRT38K5982BSSV",
+    "name": "Fridge Samsung 380 liters RT38K5982BSSV",
+    "nameC": "Fridge&nbspSamsung&nbspInverter&nbspRT38K5982BSSV",
     "id": "09",
-    "price": 10000,
+    "brandS": "SamSung",
+    "price": 672853,
     "pic1": "../asset/imgProduct/TL/tlSS/tlSS1/1.jpg",
     "pic2": "../asset/imgProduct/TL/tlSS/tlSS1/2.jpg",
     "pic3": "../asset/imgProduct/TL/tlSS/tlSS1/3.jpg",
@@ -93,20 +114,22 @@ var productF = [{
     "pic5": "../asset/imgProduct/TL/tlSS/tlSS1/5.jpg"
 
 }, {
-    "name": "Smart Television QNED LG 4K 86 inch 86QNED91TPA",
-    "nameC" :"Television&nbspLG&nbspInverter&nbsp86QNED91TPA",
+    "name": "Television LG 4K 86 inch 86QNED91TPA",
+    "nameC": "Television&nbspLG&nbspInverter&nbsp86QNED91TPA",
     "id": "10",
-    "price": 10000,
+    "brandS": "LG",
+    "price": 444250,
     "pic1": "../asset/imgProduct/TV/tvLG/tvLG3/1.jpg",
     "pic2": "../asset/imgProduct/TV/tvLG/tvLG3/2.jpg",
     "pic3": "../asset/imgProduct/TV/tvLG/tvLG3/3.jpg",
     "pic4": "../asset/imgProduct/TV/tvLG/tvLG3/4.jpg",
     "pic5": "../asset/imgProduct/TV/tvLG/tvLG3/5.jpg"
 }, {
-    "name": "Washing Machine LG Inverter 9 KG FM1209N6W",
-    "nameC" :"Washing-Machine&nbspLG&nbspInverter&nbspFM1209N6W",
+    "name": "Washing Machine LG 9 KG FM1209N6W",
+    "nameC": "Washing-Machine&nbspLG&nbspInverter&nbspFM1209N6W",
     "id": "11",
-    "price": 10000,
+    "brandS": "LG",
+    "price": 38762,
     "pic1": "../asset/imgProduct/MG/mgLG/mgLG2/1.jpg",
     "pic2": "../asset/imgProduct/MG/mgLG/mgLG2/2.jpg",
     "pic3": "../asset/imgProduct/MG/mgLG/mgLG2/3.jpg",
@@ -114,9 +137,10 @@ var productF = [{
     "pic5": "../asset/imgProduct/MG/mgLG/mgLG2/5.jpg"
 }, {
     "name": "Fridge LG Inverter 494 liters GR-D22MB",
-    "nameC" :"Fridge&nbspLG&nbspInverter&nbspGR-D22MB",
+    "nameC": "Fridge&nbspLG&nbspInverter&nbspGR-D22MB",
     "id": "12",
-    "price": 10000,
+    "brandS": "LG",
+    "price": 1941735,
     "pic1": "../asset/imgProduct/TL/tlLG/tlLG3/1.jpg",
     "pic2": "../asset/imgProduct/TL/tlLG/tlLG3/2.jpg",
     "pic3": "../asset/imgProduct/TL/tlLG/tlLG3/3.jpg",
@@ -139,11 +163,15 @@ function displayImageF(pDuctF) {
         y += `<div class="featuredP1">
         <div class="imgFP1">
             <a href="../productDetail/detail.html?key=${v.id}"><img src="${v.pic1}" alt="" loading="lazy" class="imgProductA">
+            <div class="col-sm-12">
+                <h6>${v.name}</h6>
+                </div>
+                </a>
         </div>
         <div class="compare-buyFP">
             <div class="row">
                 <div class="col-sm-6">
-                    <a href="" class="btn btn-lg btn-block">Compare</a>
+                <a href="#" data-toggle="modal" data-target="#exampleModal" class="btn btn-lg btn-block">Compare</a>
                 </div>
                 <div class="col-sm-6"><a href="" class="add-to-cart btn btn-lg btn-block" data-name="${v.nameC}" data-price="${v.price}">Buy</a></div>
             </div>
@@ -220,7 +248,7 @@ function showDetailF(pDetail) {
                     </div>
                     <div class="addToCart">
                             <a href="#" class="add-to-cart btn btn-dark" data-name="${v.nameC}" data-price="${v.price}">Add to cart</a>
-                            <a href="#" class="btn btn-dark">Compare</a>
+                            <a href="#" data-toggle="modal" data-target="#exampleModal" class="btn btn-dark">Compare</a>
                             <a href="#" class="btn btn-dark">Add review</a>
                         </div>
                 </div>
@@ -239,11 +267,15 @@ function displayImageS(pDuctH) {
         x += `<div class="featuredP1">
         <div class="imgFP1">
             <a href="../productDetail/detail.html?key=${v.id}"><img src="${v.pic1}" alt="" loading="lazy" class="imgProductA">
+            <div class="col-sm-12">
+                <h6>${v.name}</h6>
+                </div>
+                </a>
         </div>
         <div class="compare-buyFP">
             <div class="row">
                 <div class="col-sm-6">
-                    <a href="" class="btn btn-lg btn-block">Compare</a>
+                <a href="#" data-toggle="modal" data-target="#exampleModal" class="btn btn-lg btn-block">Compare</a>
                 </div>
                 <div class="col-sm-6"><a href="" class="add-to-cart btn btn-lg btn-block" data-name="${v.nameC}" data-price="${v.price}">Buy</a></div>
             </div>
@@ -252,6 +284,49 @@ function displayImageS(pDuctH) {
     })
 
     $('#sanphambanchay').html(x);
+
+    let s=`<td style="background-color:white;">
+
+    </td>`;
+    $.each(pDuctH,function(k,v){
+        
+        s+=`<td style="background-color:rgba(255, 247, 247, 0.815)">
+                                                
+     <b>${v.name}</b> 
+    
+</td>`;
+    })
+    $('.nameCompare').html(s);
+
+    let a =`<th class="text-uppercase">Label</th>`;
+    $.each(pDuctH,function(k,v){
+        
+        a+=`<td><span class="text-dark font-weight-semibold">${v.brandS}</span>
+        </td>`;
+    })
+    $(".lableCompare").html(a);
+    let b =`<th>Size</th>`;
+    $.each(pDuctH,function(k,v){
+        
+        b+=`<td><span class="text-dark font-weight-semibold">${v.size}</span>
+        </td>`;
+    })
+    $(".sizeCompare").html(b);
+    let c =`<th>Relase Year</th>`;
+    $.each(pDuctH,function(k,v){
+        
+        c+=`<td><span class="text-dark font-weight-semibold">${v.year}</span>
+        </td>`;
+    })
+    $(".relaseYear").html(c);
+    let d =`<th>Price</th>`;
+    $.each(pDuctH,function(k,v){
+        
+        d+=`<td><span class="text-dark font-weight-semibold">$${v.price}</span>
+        </td>`;
+    })
+    $(".priceCompare").html(d);
+
 }
 displayImageS(productS);
 
@@ -319,10 +394,10 @@ function showDetail(pDetail) {
                         <h3>Price: $${v.price}</h3>
                     </div>
                     <div class="addToCart">
-                        <a href="#" class="btn btn-dark">Add to cart</a>
-                        <a href="#" class="btn btn-dark">Compare</a>
-                        <a href="#" class="btn btn-dark">Add review</a>
-                    </div>
+                            <a href="#" class="add-to-cart btn btn-dark" data-name="${v.nameC}" data-price="${v.price}">Add to cart</a>
+                            <a href="#" data-toggle="modal" data-target="#exampleModal" class="btn btn-dark">Compare</a>
+                            <a href="#" class="btn btn-dark">Add review</a>
+                        </div>
                 </div>
             </div>
         </div>
